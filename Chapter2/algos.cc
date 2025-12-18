5f39518155303de6ee531426186b3da791ec471f
Carlos <110690378+csalas-alarcon@users.noreply.github.com>
2025-12-18 14:24:03 +0100

    Finished Chapter 2

--- Chapter2/algos.cc
+++ Chapter2/algos.cc
@@ -24,3 +24,85 @@ int multiply_by_15(int a) {
     int c = b + b;
     return (c + c) + b;
 }
+
+// Multiply Accumulative Algo .1v
+
+int mult_acc0( int r, int n, int a) {
+    if (n==1) return r + a;
+    if (odd(n)) {
+        return mult_acc0( r + a, half(n), a + a );
+    } else {
+        return mult_acc0( r, half(n), a + a);
+    }
+}
+
+// Multiply Accumulative Algo .2v
+
+int mult_acc1( int r, int n, int a) {
+    if ( n==1 ) return r +a;
+    if (odd(n)) r = r + a;
+    return mult_acc1(r, half(n), a + a);
+}
+
+// Multiply Accumulative Algo .3v
+
+int mult_acc2( int r, int n, int a) {
+    if (odd(n)) {
+        r = r + a;
+        if (n ==1) return n;
+    }
+    return mult_acc2( r, half(n), a + a);
+}
+
+// Multiply Accumulative Algo .4v
+
+int mult_acc3( int r, int n, int a) {
+    if (odd(n)) {
+        r = r + a;
+        if (n==1) return r;
+    }
+    n = half(n);
+    a = a + a;
+    return mult_acc3( r, n , a);
+}
+
+// Multiply Accumulative Algo .5v
+
+int mult_acc4( int r, int n, int a) {
+    while (true) {
+        if (odd(n)) {
+            r = r + a;
+            if (n==1) return r;
+        }
+        n = half(n);
+        a = a + a;
+    }
+}
+
+// Multiply w Integrated Accumulative Function Algo .1v
+
+int multiply2( int n, int a) {
+    if (n==1) return a;
+    return mult_acc4( a, n+1, a);
+}
+
+// Multiply w Integrated Accumulative Function Algo .2v
+
+int multiply3( int n, int a) {
+    while (!odd(n)) {
+        a = a + a;
+        n = half(n);
+    }
+    if (n==1) return a;
+    return mult_acc4( a, n-1, a);
+}
+
+// Multiply w Integrated Accumulative function Algo .2v
+int multiply4( int n, int a) {
+    while (!odd(n)) {
+        a = a + a;
+        n = half(n);
+    }
+    if (n==1) return a;
+    return mult_acc4( a, half(n-1), a + a);
+}
